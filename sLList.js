8a164ac56352b86ac793cef1c9a6b690a707de6a
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-05-01 17:33:17 +0530

    add method
    creating node
    creating Linked list
    add data to linked list

--- sLList.js
+++ sLList.js
@@ -5,6 +5,7 @@ class Node{
         this.next = null;
     }
 }
+
 class LinkedList{
     constructor(head){
         this.head = null;
@@ -13,15 +14,31 @@ class LinkedList{
     // methods
     //add
     add(element){
-        var node = new Node();
+        var node = new Node(element);
         // if the linedlist is empty
         if(!this.head){
             this.head = node;
         }else{
-            
-        }
-    }
-}
+            var current = this.head; // pointer current is allocated to head    //
+            while(current.next){        // search karo ki current ka next kabh null hota hai
+                current  = current.next;  
+            }
 
+            /*  while-loop will terminate only when it will reach to last node
+             and tabhi curr.next ko  null mil jata hai so the while-loop will terminate  */
+            current.next = node;    //  last node me naya node add karo
+        }
+        this.size++;            
+        }
+
+    }
+    let list = new LinkedList();
+    console.log('list:', list)
+    list.add(2);
+    console.log('list:', list)
+    list.add(21);
+
+
+    console.log('list:', list)
 
 // console.log(!null); // true
